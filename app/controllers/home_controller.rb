b01f5946c0d6d42bd9ed9d3064582a5f4c97174a
Jimmy Perrine <jimmy.perrine@gmail.com>
2011-07-21 16:00:51 -0400

    COMMENTS

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,18 @@
 class HomeController < ApplicationController
   
+  # put in email here
   def new
     
   end
   
+  # email gets posted here
   def create
     gmail_contacts = GmailContacts.new
     url = gmail_contacts.authsub_url "http://localhost:3000/home/contacts?email=#{params[:email]}"
     redirect_to url
   end
 
+  # gmail posts back here, as long as the user selects the same account as they entered it will work
   def contacts
     gmail_contacts = GmailContacts.new params['token'].to_s
 
